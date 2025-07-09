075d433c02bb0a54626f519170ba27c134170cfd
toTechGuru <roman.borsuk.guru@gmail.com>
2025-07-09 19:21:26 +0300

    change route

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(helmet());
 // }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://admin-pannel-brown.vercel.app',
   credentials: true
 }));
 app.options('*', cors()); // Handle preflight requests
